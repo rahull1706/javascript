c054e94cdcfde26e89d57c785c537f3f3ce89a8f
rahull1706 <72141953+rahull1706@users.noreply.github.com>
2022-05-29 19:06:17 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,68 @@
+// 
+// for(i=0;i<=10;i++)
+ // var i=21;
+// if(i%2===0){
+//     console.log(i+"-->even");
+// }
+// else{
+//     console.log(i+"-->odd");
+// }
+// }
+// for (let j=0;j<=20;j+=2){
+//     console.log(j);
+// }
+// let d1=1
+// for(i=0;i<=30;i++){
+//     d1 = d1+d1
+//     console.log(d1);
+// }
+// console.log(d1);
+// d1=d1/100
+// console.log(d1);
+// var length=10;
+// var breadth=20;
+// if (length===breadth){
+//     console.log("the shape is a square");
+// }
+// else{
+//     console.log("the shape is a rectangle");
+// }
+var day=3;
+switch(day){
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednsday");
+        break;
+    case 5:
+        console.log("thursday");
+        break;
+    case 6:
+        console.log("friday");
+        break;
+    case 7:
+        console.log("saturday");
+        break;
+    default:
+        console.log("invalid day number");
+        break;
+
+}
+
+ 
+
+
+
+
+
+
+
+
+ 
